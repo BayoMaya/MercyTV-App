b6cc33ac44dd878f88f61539a69396bbd5028460
Olatunji Ogunkomaya <teejaymayaboss@gmail.com>
2019-07-16 18:19:35 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import SplashScreen from 'react-native-splash-screen';
+
+import MainNavigator from './src/components/screens/Navigations';
+
+class App extends React.Component {
+  componentDidMount() {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <MainNavigator />
+    );
+  }
+}
+
+/**
+const App = () => {
+  return (
+    <MainNavigator />
+  );
+};
+**/
+
+export default App;
